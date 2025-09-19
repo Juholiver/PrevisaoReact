efdf74795fd84bfd9ea8521eccfd3d8d5a3aa06b
Juholiver <juholiver3@gmail.com>
2025-09-19 15:25:28 -0300

    criando a funcao searchCity

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { useState, useRef } from 'react'
+import axios from 'axios'
 
 import './App.css'
 
@@ -6,8 +7,18 @@ function App() {
   const [count, setCount] = useState(0)
   const inputRef = useRef()
 
-  function searchCity(){
+    async function searchCity(){
     console.log(inputRef.current.value)
+    const city = inputRef.current.value
+    const key = "0ba11199028d166431e519372816ff08"
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${key}&lang=pt_br&units=metric`
+  
+    try {
+      const data = await axios.get(url)
+      console.log(data)
+    } catch (error) {
+      console.log(error)
+    }
   }
 
   return (
