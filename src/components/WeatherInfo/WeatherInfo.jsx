73e684046ca9a8392ab3614e80d0412ca01a44ad
Juholiver <juholiver3@gmail.com>
2025-09-19 18:49:57 -0300

    Stylizando WeatherInfo

--- src/components/WeatherInfo/WeatherInfo.jsx
+++ src/components/WeatherInfo/WeatherInfo.jsx
@@ -1,13 +1,15 @@
+import './WeatherInfo.css'
+
 function WeatherInfo({weather}){
   return (
-    <div>
+    <div className='weather-container'> 
       <h2>{weather.name}</h2>
-      <div>
+      <div className="weather-info">
         <img src={`https://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`} alt={weather.weather[0].description} />
-        <p>{Math.round(weather.main.temp)} °C</p>
+        <p className="temp">{Math.round(weather.main.temp)} °C</p>
       </div>
-      <p>{weather.weather[0].description}</p>
-      <div>
+      <p className="description">{weather.weather[0].description}</p>
+      <div className="weather-details">
         <p>Humidade: {weather.main.humidity}%</p>
         <p>Pressão: {weather.main.pressure}</p>
         <p>Sensação Térmica: {Math.round(weather.main.feels_like)} °C</p>
